7af9738b13211d47acf502af46dac089b095a113
Aynorm <116764902+Aynorm@users.noreply.github.com>
2025-01-24 13:26:43 +0100

    Add files via upload

---
+++ latestportfoliooption.js
@@ -0,0 +1,5 @@
+import {mobileNav, navTop, bottomNav} from './latestportfolio.js';
+
+document.getElementById("mobileNav").innerHTML = mobileNav;
+document.getElementById("navTop").innerHTML = navTop;
+document.getElementById("bottomNav").innerHTML = bottomNav;
