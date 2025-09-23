17e57a6b0b6891dcf7643f18d0fcf8cafe9059f1
Aynorm <116764902+Aynorm@users.noreply.github.com>
2025-09-23 15:53:04 +0100

    Delete latestportfolio directory

--- latestportfolio/latestportfoliooption.js
+++
@@ -1,5 +0,0 @@
-import {mobileNav, navTop, bottomNav} from './latestportfolio.js';
-
-document.getElementById("mobileNav").innerHTML = mobileNav;
-document.getElementById("navTop").innerHTML = navTop;
-document.getElementById("bottomNav").innerHTML = bottomNav;
