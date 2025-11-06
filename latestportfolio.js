68a7365a95683bde9f82d407bcedab537412caf9
Aynorm <116764902+Aynorm@users.noreply.github.com>
2025-11-06 05:17:02 -0800

    Add files via upload

--- latestportfolio.js
+++ latestportfolio.js
@@ -51,21 +51,3 @@ export const bottomNav =
 ";
 
 document.getElementById("bottomNav").innerHTML = bottomNav;
-
-//Rain fall js
-var nbDrop = 858;
-function randRange(minNum, maxNum) {
-  return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-}
-
-function createRain() {
-  for (i = 1; i < nbDrop; i++) {
-    var dropLeft = randRange(0, 1600);
-    var dropTop = randRange(-1000, 1400);
-    $(".rain").append('<div class="drop" id="drop' + i + '"></div>');
-    $("#drop" + i).css("left", dropLeft);
-    $("#drop" + i).css("top", dropTop);
-  }
-}
-
-createRain();
